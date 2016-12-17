31b82c272305a6e7f0cc362edacc8f5a64545b42
Dike Tracker <d.biketracker@gmail.com>
2016-12-17 01:15:05 +0000

    connection for clearDB config - udpate

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
         //gets the variables from the url and parses them to the variables
         $url = parse_url(getenv("mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true"));
         
-        $server = $url["host"];
-        $username = $url["user"];
-        $password = $url["pass"];
+        $server = $url["us-cdbr-iron-east-04.cleardb.net"];
+        $username = $url["b4c04b4b0847ac"];
+        $password = $url["bdfbd3f7"];
         $db = substr($url["path"], 1);
         //create the conneciton
         $conn = new mysqli($server, $username, $password, $db);
