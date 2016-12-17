7f07640dd15a80592b2e085d1c68548ce71d9c40
Dike Tracker <d.biketracker@gmail.com>
2016-12-17 01:27:04 +0000

    connection for clearDB config - udpate

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
         <?php
         //connect to ClearDB
         //gets the variables from the url and parses them to the variables
-        $url = parse_url(getenv("mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true"));
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-        $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-        $username = $url["b4c04b4b0847ac"];
-        $password = $url["bdfbd3f7"];
-        $db = $url["heroku_1aebd2cf6f33fe1"];
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
         //create the conneciton
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
