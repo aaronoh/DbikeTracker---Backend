94e279b3ef74a279bbbbade07a85581a920ad8ff
Dike Tracker <d.biketracker@gmail.com>
2016-12-17 01:28:00 +0000

    connection for clearDB config - udpate

--- index.php
+++ index.php
@@ -14,14 +14,21 @@
         $password = $url["pass"];
         $db = substr($url["path"], 1);
         //create the conneciton
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
+        $conn = new mysqli($servername, $username, $password);
+
+// Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
+        echo "Connected successfully";
+//        try {
+//            $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+//            // set the PDO error mode to exception
+//            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//            echo "Connected successfully";
+//        } catch (PDOException $e) {
+//            echo "Connection failed: " . $e->getMessage();
+//        }
         ?>
     </head>
 </html>
