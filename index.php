31f4295fb0d1a4806183a5789f69a62d18106bac
Dike Tracker <d.biketracker@gmail.com>
2016-12-19 11:24:08 +0000

    attempting inputting each location

--- index.php
+++ index.php
@@ -32,26 +32,33 @@
         //convert the json to a php assoc array for query
         $dbikeinfo = json_decode($contents, true);
 //        print_r($dbikeinfo);
-        //insertion query for the locations
+//insertion query for the locations
         foreach ($allData as $key => $value) {
-            // Set the query.
-            $query = "INSERT INTO locations (NUMBER, NAME, LAT, LNG)
-VALUES ('?, ? , ? , ?')";
-
-            // Bind the params.
-            // mysqli_stmt_bind_param($query, 'ss', $value['data_id'], $value['name']);
-            mysqli_stmt_bind_param($query, 'ssss', $dbikeinfo['number'], $dbikeinfo['name'], $dbikeinfo['lat'], $dbikeinfo['lng']);
-
-            // Run the query.
+            $query = "INSERT INTO locations (number, name, lat, lng ) VALUES ('" . $dbikeinfo['number'] . "', '" . $dbikeinfo['name'] 
+                    . "', '" . $dbikeinfo['lat'] . "', '" . $dbikeinfo['lng'] . "')";
             $result = mysqli_query($mysqli, $query) or die(mysqli_error());
         }
 
 
-        if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
+//        foreach ($dbikeinfo as $locations => $value) {
+//            // Set the query.
+//            $query = "INSERT INTO locations (NUMBER, NAME, LAT, LNG)
+//VALUES ('?, ? , ? , ?')";
+//
+//            // Bind the params.
+//            // mysqli_stmt_bind_param($query, 'ss', $value['data_id'], $value['name']);
+//            mysqli_stmt_bind_param($query, 'isss', $dbikeinfo['number'], $dbikeinfo['name'], $dbikeinfo['lat'], $dbikeinfo['lng']);
+//
+//            // Run the query.
+//            $result = mysqli_query($mysqli, $query) or die(mysqli_error());
+//        }
+//
+//
+//        if (mysqli_query($conn, $query)) {
+//            echo "New record created successfully";
+//        } else {
+//            echo "Error: " . $query . "<br>" . mysqli_error($conn);
+//        }
 
         mysqli_close($conn);
         ?>
