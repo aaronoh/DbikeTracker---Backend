9d4cb321866ec48d366ba4f22d95a4b16c77dea5
Dike Tracker <d.biketracker@gmail.com>
2016-12-19 10:25:54 +0000

    added the bike data removed map for testing if live

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
         $api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
         $contents = file_get_contents($api_url);
         //convert the json to a php assoc array for query
-        $dbikeinfo = json_decode($json_array, true);
+        $dbikeinfo = json_decode($contents, true);
         print_r($dbikeinfo);
         
         ?>
