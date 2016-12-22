21a05f3bf0ed6cc49bbb35d0327d06d7747de25b
Dike Tracker <d.biketracker@gmail.com>
2016-12-22 16:58:57 +0000

    query

--- index.php
+++ index.php
@@ -1,46 +1,47 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-  //connect to ClearDB
-        //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
-        //gets the variables from the url and parses them to the variables
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//connect to ClearDB
+//cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
+//gets the variables from the url and parses them to the variables
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-        $server = $url["host"];
-        $username = $url["user"];
-        $password = $url["pass"];
-        $db = substr($url["path"], 1);
-        //create the conneciton
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+//create the conneciton
 //        $conn = new mysqli($server, $username, $password, $db);
-        $conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
+$conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 
 
-        // use prepare statement for insert query
-        $st = mysqli_prepare($conn, 'SELECT * From locations');
-		$result = mysqli_stmt_execute($st);
-		
-		$loactions = array();
-		
-		while($row = mysqli_fetch_assoc($result)){
-		$locations[] = $row;
-		}
-		mysqli_close($conn);
-		
-                echo "<pre>";
-                $locations[];
-                echo "</pre>";
+// use prepare statement for insert query
+$st = mysqli_prepare($conn, 'SELECT * From locations');
+$result = mysqli_stmt_execute($st);
 
+$loactions = array();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = mysqli_fetch_assoc($result)) {
+        $locations[] = $row;
+        echo "id: " . $row["id"] . " - number: " . $row["number"] . " -lat " . $row["lat"]  . " -lng " . $row["lng"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($conn);
         // loop through the array
         // foreach ($dbikeinfo as $row) {
             // get the locations details
