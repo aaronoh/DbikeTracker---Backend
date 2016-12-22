fe4785e19b532b3dd6bf47090862d12e175e060a
Aaron O'Hare <Aaron O'Hare>
2016-12-22 16:43:01 +0000

    Select all Locations

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-</html>
+<?php
 
-<!DOCTYPE HTML>
-<html>
-    <title>dbiketracker</title>
-    <head>
-        <?php
-        //connect to ClearDB
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+  //connect to ClearDB
         //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
         //gets the variables from the url and parses them to the variables
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -24,51 +24,35 @@
         }
         echo "Connected successfully";
 
-        //information for the bikes api
-        $api_key = "ec447add626cfb0869dd4747a7e50e21d39d1850";
-        $contract_name = "Dublin";
-        //phpinfo();
-        $api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
-        $contents = file_get_contents($api_url);
-        //convert the json to a php assoc array for query
-        $dbikeinfo = json_decode($contents, true);
 
-        //insert into availability table
-        $st = mysqli_prepare($conn, 'INSERT INTO availability(number, timeslot, avail_bikes, avail_slots, status) VALUES (?, ?, ?, ?, ?)');
-        //bind the varibales
-        mysqli_stmt_bind_param($st, 'isiis', $number, $timeslot, $avail_bikes, $avail_slot, $status);
+
+        // use prepare statement for insert query
+        $st = mysqli_prepare($conn, 'SELECT * From locations');
+		$result = mysqli_stmt_execute($st);
+		
+		$loactions = array();
+		
+		while($row = mysqli_fetch_assoc($result)){
+		$locations[] = $row
+		}
+		mysqli_close($conn)
+		
 
         // loop through the array
-        foreach ($dbikeinfo as $row) {
+        // foreach ($dbikeinfo as $row) {
             // get the locations details
-            $number = $row['number'];
-            $timeslot = strftime("%Y-%m-%d, %H:%M:%S", time());
-            $avail_bikes = $row['available_bikes'];
-            $avail_slot = $row['available_bike_stands'];
-            $status = $row['status'];
-            $timestamp = $row['last_update'];
-
-//            echo '<pre>';
-//            print_r($number);
-//            print_r($timeslot);
-//            print_r($avail_bikes);
-//            print_r($avail_slot);
-//            print_r($status);
-//            echo '</pre>';
-            // execute insert query
-            mysqli_stmt_execute($st);
-        }
-
-
-
-
-        //close connection
-        mysqli_close($conn);
-
-
-        //insert new time stamp every 10 minutes
-        //
-        
-        ?>
-    </head>
-</html>
+             // $row['number'] = $number;
+             // $row['name'] = $name;
+             // $row['position']['lat'] = $lat;
+             // $row['position']['lng'] $lng = ;
+            
+             echo '<pre>';
+             print_r($number);
+             print_r($name);
+             print_r($lat);
+             print_r($lng);
+             echo '</pre>';
+            
+            //execute insert query
+            // mysqli_stmt_execute($st);
+        // }
