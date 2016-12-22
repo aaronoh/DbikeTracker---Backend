76239b80bb884a705c01d5bb2a7b7042a6a1598c
Dike Tracker <d.biketracker@gmail.com>
2016-12-22 20:52:33 +0000

    day as int

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
-        <?php
-        //connect to ClearDB
-        //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
-        //gets the variables from the url and parses them to the variables
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+<?php
 
-        $server = $url["host"];
-        $username = $url["user"];
-        $password = $url["pass"];
-        $db = substr($url["path"], 1);
-        //create the conneciton
+//connect to ClearDB
+//cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
+//gets the variables from the url and parses them to the variables
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+//create the conneciton
 //        $conn = new mysqli($server, $username, $password, $db);
-        $conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
+$conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
-        //information for the bikes api
-        $api_key = "ec447add626cfb0869dd4747a7e50e21d39d1850";
-        $contract_name = "Dublin";
-        //phpinfo();
-        $api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
-        $contents = file_get_contents($api_url);
-        //convert the json to a php assoc array for query
-        $dbikeinfo = json_decode($contents, true);
+//information for the bikes api
+$api_key = "ec447add626cfb0869dd4747a7e50e21d39d1850";
+$contract_name = "Dublin";
+//phpinfo();
+$api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
+$contents = file_get_contents($api_url);
+//convert the json to a php assoc array for query
+$dbikeinfo = json_decode($contents, true);
 
 //        //insert into availability table
 //        $st = mysqli_prepare($conn, 'INSERT INTO availability(number, timeslot, avail_bikes, avail_slots, status) VALUES (?, ?, ?, ?, ?)');
@@ -52,35 +53,35 @@
 //            // execute insert query
 //            mysqli_stmt_execute($st);
 //        }
-        //close connection
+//close connection
 //        mysqli_close($conn);
+//insert new time stamp every 10 minutes
+$st = mysqli_prepare($conn, 'INSERT INTO times(time, dayofwk) VALUES (?, ?)');
+//bind the varibales
+mysqli_stmt_bind_param($st, 'ss', $time, $day);
+
+//close connection
+mysqli_close($conn);
 
 
-        //insert new time stamp every 10 minutes
-        $st = mysqli_prepare($conn, 'INSERT INTO times(time, dayofwk) VALUES (?, ?)');
-        //bind the varibales
-        mysqli_stmt_bind_param($st, 'ss', $time, $day);
+$epoch = strtotime('now');
+$dt = new DateTime("@$epoch");  // convert UNIX timestamp to PHP DateTime
+echo $dt->format('Y-m-d'); // output = 2017-01-01 00:00:00
+$day = date('w', $timestamp);
 
-        //close connection
-        mysqli_close($conn);
-        
-        echo "today is " . $day;
-        $timestamp = strtotime('now');
-        $day = date('w', $timestamp);
-        
-        
-        
-        $currdate = date('m/d/Y h:i:s a', time());
-        $datetime = new DateTime($currdate);
-        $date = $datetime->format('Y-m-d');
-        $time = $datetime->format('H:i:s');
-        
-        
-        echo "Todays date is " . $date;
-        echo "The time is " . $time;
-        
+
+
+$currdate = date('m/d/Y h:i:s a', time());
+$datetime = new DateTime($currdate);
+$date = $datetime->format('Y-m-d');
+$time = $datetime->format('H:i:s');
+
+
+echo "Todays date is " . $date;
+echo "The time is " . $time;
+
 //   
 //        
 //        // execute insert query
 //        mysqli_stmt_execute($st);
-        ?>
+?>
