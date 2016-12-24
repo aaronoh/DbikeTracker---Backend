0ac1b4cc357872f42aea89829e57fafb91bc30de
Aaron O'Hare <Aaron O'Hare>
2016-12-24 13:53:33 +0000

    echo json result

--- view-locations-sql.php
+++ view-locations-sql.php
@@ -9,7 +9,6 @@
         //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
         //gets the variables from the url and parses them to the variables
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-          echo "test";
         $server = $url["host"];
         $username = $url["user"];
         $password = $url["pass"];
@@ -17,49 +16,24 @@
         //create the conneciton
 //        $conn = new mysqli($server, $username, $password, $db);
         $conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
-  echo "test1";
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         
-              echo "test2";
         }
-          echo "test3";
         echo "Connected successfully";
 
 
 
         // use prepare statement for insert query
         $st = mysqli_prepare($conn, 'SELECT * From locations');
-	  echo "test4";	
         $result = mysqli_stmt_execute($st);
-  echo "test5";
 		$loactions = array();
-  echo "test6";
 		while($row = mysqli_fetch_assoc($result)){
 		$locations[] = $row;
-    echo "test6";
 		}
-  echo "test7";
                 mysqli_close($conn);
-  echo "test8";
+                
+                echo $_GET['jsoncallback'] . '(' . json_encode($result) . ');';
 
-        // loop through the array
-        // foreach ($dbikeinfo as $row) {
-            // get the locations details
-             // $row['number'] = $number;
-             // $row['name'] = $name;
-             // $row['position']['lat'] = $lat;
-             // $row['position']['lng'] $lng = ;
-
-            // echo '<pre>';
-            // print_r($number);
-            // print_r($name);
-            // print_r($lat);
-            // print_r($lng);
-            // echo '</pre>';
-
-            //execute insert query
-            // mysqli_stmt_execute($st);
-        // }
 ?>
