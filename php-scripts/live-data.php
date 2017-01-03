830a2aee0a255cad3ab6f9436b0e906b3a7df274
Dike Tracker <d.biketracker@gmail.com>
2017-01-03 13:29:09 +0000

    remove json encode

--- php-scripts/live-data.php
+++ php-scripts/live-data.php
@@ -1,5 +1,5 @@
 <?php
 $api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
 $contents = file_get_contents($api_url);
-echo json_encode($contents);
+echo ($contents);
 ?>
