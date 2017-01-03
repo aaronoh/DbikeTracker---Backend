e4034d1aa0bf30f7dd6622da1bb08ceda4914380
Aaron O'Hare <aaronoh96@gmail.com>
2017-01-03 16:20:18 +0000

    re-enable select*locations for cordova

--- view-locations-sql.php
+++ view-locations-sql.php
@@ -1,4 +1,5 @@
 <?php
+
 //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"];
@@ -11,39 +12,36 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-//$data=array();
-//$q=mysqli_query($conn,"select * from `locations`");
-//while ($row=mysqli_fetch_object($q)){
-// $data[]=$row;
-//}
-//echo json_encode($data);
-
-
 $api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
 $contents = file_get_contents($api_url);
 $dbikeinfo = json_decode($contents, true);
-        //insert into availability table
-        //$st = mysqli_prepare($conn, 'INSERT INTO live_data(number, avail_bikes, avail_slots) VALUES (?, ?, ?)');
-        $st = mysqli_prepare($conn, 'UPDATE live_data SET avail_bikes = ?, avail_slots = ? WHERE number = ?');
-        //bind the varibales
-        mysqli_stmt_bind_param($st, 'iii', $avail_bikes, $avail_slots, $number);
+$st = mysqli_prepare($conn, 'UPDATE live_data SET avail_bikes = ?, avail_slots = ? WHERE number = ?');
+//bind the varibales
+mysqli_stmt_bind_param($st, 'iii', $avail_bikes, $avail_slots, $number);
 
-        // loop through the array
-        foreach ($dbikeinfo as $row) {
-            // get the locations details
-            $number = $row['number'];
-            $avail_bikes = $row['available_bikes'];
-            $avail_slots = $row['available_bike_stands'];
+// loop through the array
+foreach ($dbikeinfo as $row) {
+    // get the locations details
+    $number = $row['number'];
+    $avail_bikes = $row['available_bikes'];
+    $avail_slots = $row['available_bike_stands'];
 
-            echo '<pre>';
-            print_r($number);
-            print_r($avail_bikes);
-            print_r($avail_slots);
-            echo '</pre>';
-             //execute insert query
-            mysqli_stmt_execute($st);
-        }
-        //close connection
-        mysqli_close($conn);
+    echo '<pre>';
+    print_r($number);
+    print_r($avail_bikes);
+    print_r($avail_slots);
+    echo '</pre>';
+    //execute update query
+    mysqli_stmt_execute($st);
+}
 
+
+$data = array();
+$q = mysqli_query($conn, "select * from `locations`");
+while ($row = mysqli_fetch_object($q)) {
+    $data[] = $row;
+}
+echo json_encode($data);
+//close connection
+mysqli_close($conn);
 ?>
