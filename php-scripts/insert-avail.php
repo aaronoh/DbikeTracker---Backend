1368c162676243099d568e9b1540d7e5f04511cf
Dike Tracker <d.biketracker@gmail.com>
2017-01-03 13:44:14 +0000

    attempt timestamp

--- php-scripts/insert-avail.php
+++ php-scripts/insert-avail.php
@@ -1,4 +1,5 @@
 <?php
+
 //connect to ClearDB
 //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
 //gets the variables from the url and parses them to the variables
@@ -51,7 +52,7 @@ $dbikeinfo = json_decode($contents, true);
 //$sqlday = $timearray[2][i]; //access the date part of the array
 
 
-  
+
 
 
 $epoch = strtotime('now');
@@ -65,43 +66,47 @@ $day = date('w');
 
 
 //compare the time we just got to a time variable like NOW()/timeofdy;
+//insert into availability table
+$st = mysqli_prepare($conn, 'INSERT INTO availability(number, timeslot, avail_bikes, avail_slots, status, last_update) VALUES (?, ?, ?, ?, ?, ?)');
+//bind the varibales
+mysqli_stmt_bind_param($st, 'isiisi', $number, $timeslot, $avail_bikes, $avail_slot, $status, $last_update);
 
 
-        //insert into availability table
-        $st = mysqli_prepare($conn, 'INSERT INTO availability(number, timeslot, avail_bikes, avail_slots, status, last_update) VALUES (?, ?, ?, ?, ?, ?)');
-        //bind the varibales
-        mysqli_stmt_bind_param($st, 'isiisi', $number, $timeslot, $avail_bikes, $avail_slot, $status, $last_update);
-        
-        
-        //check times
+//check times
 //        
-        $timestamp = strtotime($row['last_update']);
-        $update_time = new DateTime($timestamp);
+$timestamp = strtotime($row['last_update']);
+$update_time = new DateTime($timestamp);
 //        $update_time->format('Y-m-d H:i:s');
-        print_r($update_time);
-        
+print_r($update_time);
 
-        // loop through the array
-        foreach ($dbikeinfo as $row) {
-            // get the locations details
-            $number = $row['number'];
+
+
+// loop through the array
+foreach ($dbikeinfo as $row) {
+    $dt = DateTime::createFromFormat("d.m.Y H:i:s", $row['last_update']);
+if ($dt === false) {
+    throw new Exception("Invalid date");
+}
+$dt->format("Y-m-d H:i:s");
+    // get the locations details
+    $number = $row['number'];
 //            $timeslot = ;
-            $avail_bikes = $row['available_bikes'];
-            $avail_slot = $row['available_bike_stands'];
-            $status = $row['status'];
-            $last_update = date('Y-m-d H:i:s');
+    $avail_bikes = $row['available_bikes'];
+    $avail_slot = $row['available_bike_stands'];
+    $status = $row['status'];
+    $last_update = $dt;
 
-            echo '<pre>';
-            print_r($number);
-            print_r($timeslot);
-            print_r($avail_bikes);
-            print_r($avail_slot);
-            print_r($status);
-            print_r($last_update);
-            echo '</pre>';
-             //execute insert query
-            mysqli_stmt_execute($st);
-        }
+    echo '<pre>';
+    print_r($number);
+    print_r($timeslot);
+    print_r($avail_bikes);
+    print_r($avail_slot);
+    print_r($status);
+    print_r($last_update);
+    echo '</pre>';
+    //execute insert query
+    mysqli_stmt_execute($st);
+}
 //        mysqli_stmt_execute($gettime);
 //close connection
-        mysqli_close($conn);
+mysqli_close($conn);
