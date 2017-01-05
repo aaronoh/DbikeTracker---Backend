db9ba2ce15240a66f2aa73452c34ad8370cc0c61
Dike Tracker <d.biketracker@gmail.com>
2017-01-05 19:13:21 +0000

    major checks

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -92,8 +92,8 @@ if (mysqli_num_rows($result) > 0) {
         $i = 0;
         if ($last_update[$i] == $timeofdy[$i] && $dayofwk[$i] == $arrayofdays[$i]) {
             
-            echo "<br> dayofwk: " . $dayofwk . " - arrayofdays: " . $arrayofdays . "</br>";
-            echo "<br> lastupdate: " . $last_update . " - timeofdy: " . $timeofdy . "</br>";
+            echo "<br> dayofwk: " . $dayofwk[$i] . " - arrayofdays: " . $arrayofdays[$i] . "</br>";
+            echo "<br> lastupdate: " . $last_update[$i] . " - timeofdy: " . $timeofdy[$i] . "</br>";
 ////            echo "<br> timesid: ". $row["TIMES_ID"]. " - dayofwk1: ". $row['DAYOFWKAV'] . " - dayofwk2: ". $row["DAYOFWK"] .  " - lastupdate: ". $row["LAST_UPDATE"] ." - timeofdy: ". $row["TIMEOFDY"] . "<br>";
             $timeslot_query = mysqli_prepare($conn, "UPDATE availability SET TIMESLOT = ? WHERE DAYOFWK = $arrayofdays");
 //         mysqli_prepare($conn, 'INSERT INTO timeslotjunc(TIMES_ID) VALUES(?)');
