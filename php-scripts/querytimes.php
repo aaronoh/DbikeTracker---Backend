016d81e06c5feeaefc22c324d516e2c704de9097
Dike Tracker <d.biketracker@gmail.com>
2017-01-05 18:39:03 +0000

    major checks

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -93,14 +93,14 @@ if (mysqli_num_rows($result) > 0) {
         if ($last_update[$i] == $timeofdy[$i] && $dayofwk[$i] == $arrayofdays[$i]) {
             echo "<br> dayofwk: " . $dayofwk . " - arrayofdays: " . $arrayofdays . "</br>";
             echo "<br> lastupdate: " . $last_update . " - timeofdy: " . $timeofdy . "</br>";
-//////            echo "<br> timesid: ". $row["TIMES_ID"]. " - dayofwk1: ". $row['DAYOFWKAV'] . " - dayofwk2: ". $row["DAYOFWK"] .  " - lastupdate: ". $row["LAST_UPDATE"] ." - timeofdy: ". $row["TIMEOFDY"] . "<br>";
-//            $timeslot_query = mysqli_prepare($conn, "UPDATE availability SET TIMESLOT = ? WHERE DAYOFWK = $dayofwk");
-////         mysqli_prepare($conn, 'INSERT INTO timeslotjunc(TIMES_ID) VALUES(?)');
-/////        bind the varibales
-//            mysqli_stmt_bind_param($timeslot_query, 'i', $row['TIMES_ID']);
-//            echo "<br> timeody: " . $timeofdy . " - lastupdate: " . $last_update . "</br>";
-//            //         execute insert query
-//            mysqli_stmt_execute($timeslot_query);
+////            echo "<br> timesid: ". $row["TIMES_ID"]. " - dayofwk1: ". $row['DAYOFWKAV'] . " - dayofwk2: ". $row["DAYOFWK"] .  " - lastupdate: ". $row["LAST_UPDATE"] ." - timeofdy: ". $row["TIMEOFDY"] . "<br>";
+            $timeslot_query = mysqli_prepare($conn, "UPDATE availability SET TIMESLOT = ? WHERE DAYOFWK = $dayofwk");
+//         mysqli_prepare($conn, 'INSERT INTO timeslotjunc(TIMES_ID) VALUES(?)');
+///        bind the varibales
+            mysqli_stmt_bind_param($timeslot_query, 'i', $row['TIMES_ID']);
+            echo "<br> timeody: " . $timeofdy . " - lastupdate: " . $last_update . "</br>";
+            //         execute insert query
+            mysqli_stmt_execute($timeslot_query);
             $i++;
         }
 
