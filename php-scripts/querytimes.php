e7b8ecdfb30140e378c7e11e009a09260a116f2b
Dike Tracker <d.biketracker@gmail.com>
2017-01-05 18:50:22 +0000

    major checks

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -90,7 +90,7 @@ if (mysqli_num_rows($result) > 0) {
 //        echo "<br> LAST UPDATE  " . $last_update . "</br>";
 //        echo "<br> TIME OF DAY " . $timeofdy . "</br>";
         $i = 0;
-        if ($last_update == $timeofdy && $arrayofdays == $dayofwk[$i]) {
+        if ($last_update == $timeofdy && $dayofwk[$i] == $arrayofdays) {
             echo "<br> dayofwk: " . $dayofwk . " - arrayofdays: " . $arrayofdays . "</br>";
             echo "<br> lastupdate: " . $last_update . " - timeofdy: " . $timeofdy . "</br>";
 ////            echo "<br> timesid: ". $row["TIMES_ID"]. " - dayofwk1: ". $row['DAYOFWKAV'] . " - dayofwk2: ". $row["DAYOFWK"] .  " - lastupdate: ". $row["LAST_UPDATE"] ." - timeofdy: ". $row["TIMEOFDY"] . "<br>";
@@ -98,7 +98,6 @@ if (mysqli_num_rows($result) > 0) {
 //         mysqli_prepare($conn, 'INSERT INTO timeslotjunc(TIMES_ID) VALUES(?)');
 ///        bind the varibales
             mysqli_stmt_bind_param($timeslot_query, 'i', $row['TIMES_ID']);
-            echo "<br> timeody: " . $timeofdy . " - lastupdate: " . $last_update . "</br>";
             //         execute insert query
             mysqli_stmt_execute($timeslot_query);
             $i++;
