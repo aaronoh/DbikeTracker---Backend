54d3f9740d918859acb6222ee6c1056762bc97c9
Dike Tracker <d.biketracker@gmail.com>
2017-01-05 17:51:58 +0000

    try insert times id to avail

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -87,7 +87,7 @@ if (mysqli_num_rows($result) > 0) {
 
 
 
-        if ($last_update == $timeofdy && $dayofwk == $tdayofwk) {
+        if ($last_update == $timeofdy && $dayofwk == $day) {
             echo "<br> dayofwk: " . $dayofwk . " - daynow: " . $day . "</br>";
 ////            echo "<br> timesid: ". $row["TIMES_ID"]. " - dayofwk1: ". $row['DAYOFWKAV'] . " - dayofwk2: ". $row["DAYOFWK"] .  " - lastupdate: ". $row["LAST_UPDATE"] ." - timeofdy: ". $row["TIMEOFDY"] . "<br>";
             $timeslot_query = mysqli_prepare($conn, "UPDATE availability SET TIMESLOT = ? WHERE DAYOFWK = $dayofwk");
