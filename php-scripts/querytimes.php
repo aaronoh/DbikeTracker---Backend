184355767336d03b483f0218f8bb1636326649fe
Dike Tracker <d.biketracker@gmail.com>
2017-01-05 17:39:38 +0000

    try insert times id to avail

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -72,7 +72,7 @@ $data = array();
 //$q = mysqli_query($conn, "SELECT * FROM TIMES");
 $timesquery = "SELECT times.* , availability.`LAST_UPDATE`, availability.`DAYOFWK` as DAYOFWKAV 
 FROM TIMES JOIN availability
-ON  availability.LAST_UPDATE = times.TIMEOFDY";
+ON   times.TIMEOFDY = availability.LAST_UPDATE";
 
 
 $result = $conn->query($timesquery);
