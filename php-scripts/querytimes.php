cbded3e592857c2cac05b54d46368b8dd6df1725
Dike Tracker <d.biketracker@gmail.com>
2017-01-05 19:05:44 +0000

    major checks

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -95,7 +95,7 @@ if (mysqli_num_rows($result) > 0) {
             echo "<br> dayofwk: " . $dayofwk . " - arrayofdays: " . $arrayofdays . "</br>";
             echo "<br> lastupdate: " . $last_update . " - timeofdy: " . $timeofdy . "</br>";
 ////            echo "<br> timesid: ". $row["TIMES_ID"]. " - dayofwk1: ". $row['DAYOFWKAV'] . " - dayofwk2: ". $row["DAYOFWK"] .  " - lastupdate: ". $row["LAST_UPDATE"] ." - timeofdy: ". $row["TIMEOFDY"] . "<br>";
-            $timeslot_query = mysqli_prepare($conn, "UPDATE availability SET TIMESLOT = ? WHERE DAYOFWK = $dayofwk");
+            $timeslot_query = mysqli_prepare($conn, "UPDATE availability SET TIMESLOT = ? WHERE DAYOFWK = $arrayofdays");
 //         mysqli_prepare($conn, 'INSERT INTO timeslotjunc(TIMES_ID) VALUES(?)');
 ///        bind the varibales
             mysqli_stmt_bind_param($timeslot_query, 'i', $row['TIMES_ID']);
