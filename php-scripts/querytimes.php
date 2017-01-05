c873d5a388651e19e80290ae6723417dc4864db1
Dike Tracker <d.biketracker@gmail.com>
2017-01-05 19:09:10 +0000

    major checks

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -90,7 +90,7 @@ if (mysqli_num_rows($result) > 0) {
 //        echo "<br> LAST UPDATE  " . $last_update . "</br>";
 //        echo "<br> TIME OF DAY " . $timeofdy . "</br>";
         $i = 0;
-        if ($last_update == $timeofdy && $arrayofdays == $dayofwk) {
+        if ($last_update == $timeofdy && $dayofwk == $arrayofdays) {
             
             echo "<br> dayofwk: " . $dayofwk . " - arrayofdays: " . $arrayofdays . "</br>";
             echo "<br> lastupdate: " . $last_update . " - timeofdy: " . $timeofdy . "</br>";
