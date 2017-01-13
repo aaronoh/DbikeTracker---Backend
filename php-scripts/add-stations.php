5c00d3d698ef5841ded88d4ebdf678c2aaf67d0e
BowbiOner <bowbione@gmail.com>
2017-01-13 00:53:54 +0000

    copy of stations update

--- php-scripts/add-stations.php
+++ php-scripts/add-stations.php
@@ -1,35 +1,36 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-        $st = mysqli_prepare($conn, 'INSERT INTO stations(name, address, number) VALUES (?, ?, ?)');
-        //bind the varibales
-        mysqli_stmt_bind_param($st, 'ssi', $name, $address, $number);
+$st = mysqli_prepare($conn, 'INSERT INTO stations_copy(name, address, number, lat, lng) VALUES (?, ?, ?, ?, ?)');
+//bind the varibales
+mysqli_stmt_bind_param($st, 'ssi', $name, $address, $number);
 
-        // loop through the array
-        foreach ($dbikeinfo as $row) {
-            // get the locations details
-            $name = $row['name'];
-            $address = $row['address'];
-            $number = $row['number'];
+// loop through the array
+foreach ($dbikeinfo as $row) {
+    // get the locations details
+    $name = $row['name'];
+    $address = $row['address'];
+    $number = $row['number'];
+    $lat = $row['position']['lat'];
+    $lng = $row['position']['lng'];
 
 //            echo '<pre>';
 //            print_r($number);
 //            print_r($name);
 //            print_r($address);
 //            echo '</pre>';
-
-            // execute insert query
-            mysqli_stmt_execute($st);
-        }
-
+    // execute insert query
+    mysqli_stmt_execute($st);
+}
 
 
 
-        //close connection
-        mysqli_close($conn);
+
+//close connection
+mysqli_close($conn);
 
