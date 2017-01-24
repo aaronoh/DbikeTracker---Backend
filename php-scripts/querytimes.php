46f8cfa031642e177d7b396db4801c54157fe485
BowbiOner <bowbione@gmail.com>
2017-01-24 17:27:53 +0000

    new update query for timeslot

--- php-scripts/querytimes.php
+++ php-scripts/querytimes.php
@@ -71,7 +71,7 @@ if ($result) {
             if (($last_update == $timeofdy) && ($row['DAYOFWKAV'] == $arrayofdays)) {
                 echo "Updating! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
 
-                $timeslot_query = "UPDATE availability_new SET TIMESLOT = :timeslotVal WHERE DAYOFWK = :dayOfWeek AND LAST_UPDATE = :lastUpdate";
+                $timeslot_query = "UPDATE availability_new SET TIMESLOT = :timeslotVal WHERE DAYOFWK = :dayOfWeek AND LAST_UPDATE = :lastUpdate AND AVAIL_ID <= 100000";
                 $statement = $conn->prepare($timeslot_query);
                 $params = array('timeslotVal' => $timesid, 'dayOfWeek' => $arrayofdays, 'lastUpdate' => $timeofdy);
                 $res = $statement->execute($params);
