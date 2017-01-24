60e520beaf2bf36f5bedd95909fd60063db0b488
BowbiOner <bowbione@gmail.com>
2017-01-24 22:20:04 +0000

    going for a check on all days and gonna suss from therE

--- php-scripts/insert-timeslot.php
+++ php-scripts/insert-timeslot.php
@@ -1,48 +1,89 @@
-<html>
-    <?php
+<?php
+//connect to ClearDB
+//cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
+//gets the variables from the url and parses them to the variables
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$dayMap = array(
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+);
 //$server = "us-cdbr-iron-east-04.cleardb.net";
 //$username = "b4c04b4b0847ac";
 //$password = "bdfbd3f7";
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 //$db = "heroku_1aebd2cf6f33fe1";
-    $dsn = "mysql:host=" . $server . ";dbname=" . $db;
+$dsn = "mysql:host=" . $server . ";dbname=" . $db;
 //create the conneciton
 //        $conn = new mysqli($server, $username, $password, $db);
 //$conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
 // Connecting to mysql database
-    $conn = new PDO($dsn, $username, $password);
+$conn = new PDO($dsn, $username, $password);
 // Check for database connection error
-    if (!$conn) {
-        die("Could not connect to DB");
-    }
-    echo "Connected successfully \n";
+if (!$conn) {
+    die("Could not connect to DB");
+}
+echo "Connected successfully \n";
 //information for the bikes api
-    $api_key = "ec447add626cfb0869dd4747a7e50e21d39d1850";
-    $contract_name = "Dublin";
+$api_key = "ec447add626cfb0869dd4747a7e50e21d39d1850";
+$contract_name = "Dublin";
 //phpinfo();
-    $api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
-    $contents = file_get_contents($api_url);
+$api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=ec447add626cfb0869dd4747a7e50e21d39d1850";
+$contents = file_get_contents($api_url);
 //convert the json to a php assoc array for query
-    $dbikeinfo = json_decode($contents, true);
-    ?>
-    <head>
-        <title>attempt searched</title>
-    </head>
-    <body>
-        <form action="">
-            Time: <input type="text" name="time"><br>
-            Date: <input type="text" name="date"><br>
-            <input type="submit" value="Submit">
-        </form>
+$dbikeinfo = json_decode($contents, true);
 
 
+$epoch = strtotime('now'); //current time
+$dt = new DateTime("@$epoch");  // convert UNIX timestamp to PHP DateTime
+$tt = new DateTime("@$epoch"); //convert the epoch to UNIX time
+echo $tt->format('H:i:s'); // output = 21:06:43
+$time = date('H:i:s');
+echo $tt->format('Y-m-d'); // output = 2017-01-01
+$day = date('w');
+//
+//
+//echo " day as int is " . $day . " ";
+$data = array();
+//$q = mysqli_query($conn, "SELECT * FROM TIMES");
+$timesquery = "SELECT times.* , availability.`LAST_UPDATE`, availability.`DAYOFWK` as DAYOFWKAV 
+FROM TIMES JOIN availability
+ON   times.TIMEOFDY = availability.LAST_UPDATE";
+$result = $conn->query($timesquery);
+if ($result) {
+// if the number of rows in the result is greater than 0
+    if ($result->rowCount() > 0) {
+        // for each item in result as $row
+        foreach ($result as $row) {
+            $timesid = $row['TIMES_ID'];
+            $last_update = $row['LAST_UPDATE'];
+            $timeofdy = $row['TIMEOFDY'];
+            $dayofwk = $dayMap[$row['DAYOFWKAV']];
+            $arrayofdays = $row['DAYOFWK'];
 
-
-    </body>
-
-
-
-</html>
+            if (($dayofwk = 0) && ($last_update >= "00:00:00") ){
+                echo "Updating! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+                
+                $timeslot_query = "UPDATE availability SET TIMESLOT = :timeslotVal + 1 WHERE DAYOFWK = :dayOfWeek AND LAST_UPDATE = :lastUpdate";
+                echo $timeslot_query;
+                echo $dayofwk + $last_update;
+//                $statement = $conn->prepare($timeslot_query);
+//                $params = array('timeslotVal' => $timesid, 'dayOfWeek' => $arrayofdays, 'lastUpdate' => $timeofdy);
+//                $res = $statement->execute($params);
+//                
+               
+            }
+        }
+    }
+//close the while loop
+}
+//mysqli_close($conn);
+echo json_encode($data);
+?>
