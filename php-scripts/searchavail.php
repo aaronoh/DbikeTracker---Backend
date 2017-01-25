f3c9d5112ce04a0010fed1fa7ce8bacc226b5fbc
BowbiOner <bowbione@gmail.com>
2017-01-25 19:54:22 +0000

    added a form so that i can try select 3 params from the database

--- php-scripts/searchavail.php
+++ php-scripts/searchavail.php
@@ -1,23 +1,36 @@
 <?php
-
 //connect to ClearDB
 //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
 //gets the variables from the url and parses them to the variables
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+$dayMap = array(
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+);
+//$server = "us-cdbr-iron-east-04.cleardb.net";
+//$username = "b4c04b4b0847ac";
+//$password = "bdfbd3f7";
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
+//$db = "heroku_1aebd2cf6f33fe1";
+$dsn = "mysql:host=" . $server . ";dbname=" . $db;
 //create the conneciton
 //        $conn = new mysqli($server, $username, $password, $db);
-$conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+//$conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
+// Connecting to mysql database
+$conn = new PDO($dsn, $username, $password);
+// Check for database connection error
+if (!$conn) {
+    die("Could not connect to DB");
 }
-echo "Connected successfully";
+echo "Connected successfully \n";
 //information for the bikes api
 $api_key = "ec447add626cfb0869dd4747a7e50e21d39d1850";
 $contract_name = "Dublin";
@@ -35,16 +48,16 @@ echo $dayofwk;
 $stat_id = $_POST['stat_id'];
 echo $stat_id;
 //sql query that will use the variables entered above and return the avail of bikes and slots of the selected statino
-$searchData = $conn->prepare($conn, 'SELECT NUMBER, AVAIL_BIKES, AVAIL_SLOTS FROM availability_new WHERE LAST_UPDATE = ?  AND DAYOFWK = ? AND NUMBER = ?');
-$searchData->bind_param("sis", $time, $dayofwk, $stat_id);
-$result = $searchData->execute();
+$searchData = $conn->prepare('SELECT NUMBER, AVAIL_BIKES, AVAIL_SLOTS FROM availability_new WHERE LAST_UPDATE = :time  AND DAYOFWK = :dayint AND NUMBER = :statnum');
+$params = array('time' => $time, 'dayint' => $dayofwk, 'statnum' => $stat_id);
+$result = $searchData->execute($params);
 if ($result > 0) {
     foreach ($result as $row) {
         echo "<br/> " . $row . " </br>";
     }
 } else {
-    echo 'no joy gringo';
-}
+        throw new Exception($conn->error);
+    }
 ?>
 
 
