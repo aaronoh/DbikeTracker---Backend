978242be6b1abb7548337cbf89f9622021bc1d12
BowbiOner <bowbione@gmail.com>
2017-01-25 19:39:39 +0000

    added a form so that i can try select 3 params from the database

--- php-scripts/searchavail.php
+++ php-scripts/searchavail.php
@@ -1,33 +1,22 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$dayMap = array(
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-);
-//$server = "us-cdbr-iron-east-04.cleardb.net";
-//$username = "b4c04b4b0847ac";
-//$password = "bdfbd3f7";
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-//$db = "heroku_1aebd2cf6f33fe1";
-$dsn = "mysql:host=" . $server . ";dbname=" . $db;
-//create the conneciton
+  //connect to ClearDB
+        //cleardb url mysql://b4c04b4b0847ac:bdfbd3f7@us-cdbr-iron-east-04.cleardb.net/heroku_1aebd2cf6f33fe1?reconnect=true
+        //gets the variables from the url and parses them to the variables
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+        //create the conneciton
 //        $conn = new mysqli($server, $username, $password, $db);
-//$conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
-// Connecting to mysql database
-$conn = new PDO($dsn, $username, $password);
-// Check for database connection error
-if (!$conn) {
-    die("Could not connect to DB");
-}
-echo "Connected successfully \n";
+        $conn = mysqli_connect($server, $username, $password, $db) or die('Error in Connecting: ' . mysqli_error($conn));
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
 //information for the bikes api
 $api_key = "ec447add626cfb0869dd4747a7e50e21d39d1850";
 $contract_name = "Dublin";
@@ -39,8 +28,14 @@ $dbikeinfo = json_decode($contents, true);
 
 echo $_POST['time'];
 //$_POST['date']->format('Y-m-d'); // output = 2017-01-01
-echo $_POST['date'] = date('w');
+$dayofwk = $_POST['date'] = date('w');
+echo $dayofwk;
 echo $_POST['stat_id'];
+
+//sql query that will use the variables entered above and return the avail of bikes and slots of the selected statino
+$searchData = mysqli_prepare($conn, 'SELECT NUMBER, AVAIL_BIKES, AVAIL_SLOTS FROM availability_new WHERE LAST_UPDATE = ?  AND DAYOFWK = ? AND NUMBER = ?');
+$searchData->bind_param("sis", $_POST['time'], $dayofwk, $_POST['stat_id']);
+$result = mysqli_stmt_execute($st);
 ?>
 
 
