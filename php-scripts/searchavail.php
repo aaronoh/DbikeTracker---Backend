b08c49cdd9c02e208b7d79530a110bb99d97e20b
BowbiOner <bowbione@gmail.com>
2017-01-28 19:39:56 +0000

    trying out some query stuff

--- php-scripts/searchavail.php
+++ php-scripts/searchavail.php
@@ -32,7 +32,7 @@ $stat_id = $_POST['stat_id'];
 echo $stat_id . "<br>";
 
 
-$sql = "SELECT number, avail_bikes, avail_slots FROM availability_new WHERE DAYOFWK = '$dayofwk'";
+$sql = "SELECT number, avail_bikes, avail_slots FROM availability_new WHERE DAYOFWK = '$dayofwk' AND LAST_UPDATE = '$time'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
