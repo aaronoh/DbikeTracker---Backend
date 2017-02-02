bc838325ab02b34400b149e8da11e16a9aa2781a
BowbiOner <bowbione@gmail.com>
2017-02-02 12:34:15 +0000

    percentile example

--- php-scripts/searchavail.php
+++ php-scripts/searchavail.php
@@ -44,15 +44,15 @@ if ($result->num_rows > 0) {
         echo $row["avail_bikes"] . "\n";
         echo $row['avail_slots'] . "\n";
         print("Unsorted array:<br/>");
-        print_r($row[]);
-        arsort($row)[];
+        print_r($row[1]);
+        arsort($row[1]);
         print("<br/>");
         print("Sorted array:<br/>");
-        print_r($row[]);
+        print_r($row[1]);
         print("<br/>");
 
         $i = 0;
-        $total = count($row[]);
+        $total = count($row[1]);
         $percentiles = array();
         $previousValue = -1;
         $previousPercentile = -1;
