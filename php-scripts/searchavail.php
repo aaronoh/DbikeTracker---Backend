8470b2b91379a4d136f53c634f089975deb36a27
BowbiOner <bowbione@gmail.com>
2017-02-15 15:12:46 +0000

    array splice index 0

--- php-scripts/searchavail.php
+++ php-scripts/searchavail.php
@@ -98,7 +98,7 @@ function mypercentile($bikes,$percentile){
     //count the size of the array input
     $count = count($bikes);
     //remove the 0 index and move the next item to the new 0 index
-    unset($bikes[0]);
+    array_pop($bikes);
     //get all the indexes and multiple by the percentile
     $allindex = ($count-1)*$p;
     //get the value of the index
