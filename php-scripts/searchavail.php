2f0275af5af3505babad5c84901f68643feee824
BowbiOner <bowbione@gmail.com>
2017-02-22 23:32:32 +0000

    print avail slots

--- php-scripts/searchavail.php
+++ php-scripts/searchavail.php
@@ -45,13 +45,14 @@ if ($result->num_rows > 0) {
 //        echo $row["avail_bikes"] . "\n";
 //        echo $row['avail_slots'] . "\n";
 
-        echo "slots " . $row['avail_slots'];
-        echo "bikes " . $row['avail_bikes'];
         array_push($bikes, $row['avail_bikes']);
     }
 } else {
     echo "0 results";
 }
+
+        echo "slots " . $row['avail_slots'];
+        echo "bikes " . $row['avail_bikes'];
 echo "<pre>";
 sort($bikes);
 print_r($bikes);
