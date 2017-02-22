98dac45cb26665702c38b626fad7eab50f7751d5
BowbiOner <bowbione@gmail.com>
2017-02-22 14:43:58 +0000

    connetion moved to separate class, including in index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-include connect.php;
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+require connect.php;
 
 ?>
